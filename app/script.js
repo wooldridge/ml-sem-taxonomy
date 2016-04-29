131c0f8f5199b0e59bf8bdc242b20c2fa209d2d2
Mike Wooldridge <mike@hellion.org>
2016-04-29 10:57:03 -0700

    integrate counts into concept calls, no need for separate count calls

--- app/script.js
+++ app/script.js
@@ -2,7 +2,6 @@
  * Handle display of concepts.
  */
 function dispSub(id) {
-  console.dir(id);
   var url, cont, ref;
   if (id) {
     url = 'http://localhost:8563/narrower?q=' + id;
@@ -37,10 +36,19 @@ function dispSub(id) {
         var parts = curr.concept.value.split("/");
         var last = parts.pop();
         var html  = '<li id="' + last + '">';
-            html += '<label class="concept">' + curr.label.value + '</label>';
+            html += '<label class="concept">';
+            html += curr.label.value;
+            if (curr.num.value > 0) {
+              html += ' (' + curr.num.value + ')';
+            }
+            html += '</label>';
             html += '</li>';
         $('ul#' + ref).append(html);
-        dispNum(last);
+        if (curr.num.value > 0) {
+          $('li#' + last).addClass('closed');
+        } else {
+          $('li#' + last).addClass('empty');
+        }
       });
       $('ul#' + ref + ' li').on('click', function(event){
         $(this).toggleClass('closed');
@@ -49,38 +57,6 @@ function dispSub(id) {
         var id = this.id;
         dispSub(id);
       });
-  }).fail(function(jqXHR, textStatus) {
-    console.dir(jqXHR);
-  });
-}
-
-/**
- * Handle display of numbers.
- */
-function dispNum(id) {
-  var url, cont, ref;
-  url = 'http://localhost:8563/number?q=' + id;
-  $.ajax({
-    method: 'GET',
-    url: url,
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }
-  }).done(
-    function(data) {
-      console.log(id);
-      console.dir(data);
-      var data = JSON.parse(data);
-      var num = data.results.bindings[0].number.value;
-      if (num > 0) {
-        $('li#' + id + ' label').append(
-          ' (' + num + ')'
-        );
-        $('li#' + id).addClass('closed');
-      } else {
-        $('li#' + id).addClass('empty');
-      }
   }).fail(function(jqXHR, textStatus) {
     console.dir(jqXHR);
   });
