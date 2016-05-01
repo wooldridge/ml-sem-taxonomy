cf152b3d077e913596652a687fdbb23b6be6f830
Mike Wooldridge <mike@hellion.org>
2016-05-01 01:55:50 -0700

    API to support concept-based search

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 var config = require('./config'),
     rp = require('request-promise'),
+    marklogic = require('marklogic'),
     express = require('express');
 
 // Set up EXPRESS
 var app = express(),
     port = config.server.port,
     router = express.Router();
+
+// Set up MARKLOGIC
+var db = marklogic.createDatabaseClient({
+  host: config.host,
+  port: config.database.port,
+  user: config.auth.user,
+  password: config.auth.pass,
+  authType: 'digest'
+});
 
 app.use(express.static(__dirname + '/app/'));
 
@@ -21,11 +31,11 @@ var auth = {
   sendImmediately: false
 };
 
-// SEARCH for keywords
+// SEARCH for a KEYWORD
 app.get('/search', function(req, res){
   var q = req.query.q;
   var url  = 'http://' + config.host + ':' + config.database.port;
-      url += '/v1/search';
+      url += '/v1/search?options=taxonomy';
   var body = {
     "query": {
       "queries": [{
@@ -60,6 +70,48 @@ app.get('/search', function(req, res){
 
 });
 
+// SEARCH for a CONCEPT
+app.get('/concept', function(req, res){
+  var q = req.query.q;
+  var url  = 'http://' + config.host + ':' + config.database.port;
+      url += '/v1/graphs/sparql';
+  var body = 'PREFIX wb: <http://vocabulary.worldbank.org/taxonomy/> ' +
+             'SELECT ?uri ' +
+             'WHERE { ' +
+             '  ?uri <http://example.org/hasConcept> wb:' + q + ' . ' +
+             '} ';
+
+  var options = {
+    method: "POST",
+    url: url,
+    body: body,
+    json: true,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    auth: auth
+  };
+  rp(options)
+    .then(function (response) {
+      console.log(JSON.stringify(response, null, 2));
+      //res.json(response);
+      var uris = response.results.bindings.map(function (curr) {
+        return curr.uri.value;
+      })
+      db.documents.read({
+        uris: uris
+      }).result(
+        function(response) {
+          res.send(response);
+        },
+        function(error) { console.log(JSON.stringify(error)); }
+      )
+    })
+    .catch(function (err) {
+      console.log(JSON.stringify(err, null, 2));
+    });
+});
+
 // TOP concepts in hierarchy, with subconcept counts
 app.get('/top', function(req, res){
   var url  = 'http://' + config.host + ':' + config.database.port;
@@ -74,6 +126,45 @@ app.get('/top', function(req, res){
               '    ?concept skos:narrower ?concept2 . ' +
               '    BIND(IF(?concept2 != "", 1, 0) AS ?count) ' +
               '  } . ' +
+              '} ' +
+              'GROUP BY ?concept ' +
+              'ORDER BY ?label ';
+
+  var options = {
+    method: "POST",
+    url: url,
+    body: body,
+    headers: {
+      'Content-Type': 'application/sparql-query'
+    },
+    auth: auth
+  };
+  rp(options)
+    .then(function (response) {
+      res.send(response);
+    })
+    .catch(function (err) {
+      console.log(JSON.stringify(err, null, 2));
+    });
+
+});
+
+// TOP concepts in hierarchy, with doc counts
+app.get('/top2', function(req, res){
+  var url  = 'http://' + config.host + ':' + config.database.port;
+      url += '/v1/graphs/sparql';
+  var body =  'PREFIX skos: <http://www.w3.org/2004/02/skos/core#> ' +
+              'SELECT ?concept ?label ' +
+              '(count(distinct ?doc) as ?num) (sum(?countSubs) as ?numSubs) ' +
+              'WHERE ' +
+              '{ ' +
+              '  ?concept skos:topConceptOf ?o . ' +
+              '  ?concept skos:prefLabel ?label . ' +
+              '  ?doc <http://example.org/hasConcept> ?concept . ' +
+              '  OPTIONAL { ' +
+              '    ?concept skos:narrower ?concept2 . ' +
+              '    BIND(IF(?concept2 != "", 1, 0) AS ?countSubs) . ' +
+              '  } ' +
               '} ' +
               'GROUP BY ?concept ' +
               'ORDER BY ?label ';
@@ -149,6 +240,48 @@ app.get('/narrower', function(req, res){
               '    ?concept skos:narrower ?concept2 . ' +
               '    BIND(IF(?concept2 != "", 1, 0) AS ?count) ' +
               '  } . ' +
+              '} ' +
+              'GROUP BY $concept ' +
+              'ORDER BY ?label';
+
+  var options = {
+    method: "POST",
+    url: url,
+    body: body,
+    headers: {
+      'Content-Type': 'application/sparql-query'
+    },
+    auth: auth
+  };
+  rp(options)
+    .then(function (response) {
+      res.send(response);
+    })
+    .catch(function (err) {
+      console.log(JSON.stringify(err, null, 2));
+    });
+
+});
+
+// NARROWER concepts in hierarchy, with subconcept counts
+app.get('/narrower2', function(req, res){
+  var url  = 'http://' + config.host + ':' + config.database.port;
+      url += '/v1/graphs/sparql';
+  var q = req.query.q;
+  var body =  'PREFIX ex: <http://example.org/> ' +
+              'PREFIX wb: <http://vocabulary.worldbank.org/taxonomy/> ' +
+              'PREFIX skos: <http://www.w3.org/2004/02/skos/core#> ' +
+              'SELECT ?concept ?label ' +
+              '(count(distinct ?doc) as ?num) (sum(?countSubs) as ?numSubs) ' +
+              'WHERE ' +
+              '{ ' +
+              '  wb:' + q + ' skos:narrower ?concept . ' +
+              '  ?concept skos:prefLabel ?label . ' +
+              '  ?doc <http://example.org/hasConcept> ?concept . ' +
+              '  OPTIONAL { ' +
+              '    ?concept skos:narrower ?concept2 . ' +
+              '    BIND(IF(?concept2 != "", 1, 0) AS ?countSubs) . ' +
+              '  } ' +
               '} ' +
               'GROUP BY $concept ' +
               'ORDER BY ?label';
